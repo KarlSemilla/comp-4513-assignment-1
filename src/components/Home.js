7c0f10f4f57e1fa618198565cb999e2ab8f78dd3
KarlSemilla <karlsemilla1@gmail.com>
2020-02-23 00:21:24 -0700

    Add responsivity to background in home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,19 +6,19 @@ class Home extends React.Component {
     render(){
         return(
             <div className = 'background'>
-            <div className = 'searchBox'>
-                <h1>Movie Browser</h1>
-                <h3>Title: 
-                    <input type='text' name="titleIn"></input>
-                </h3>
-                <Link to='/default'>
-                    <button>Show Matching Movies</button>
-                </Link>
-                <Link to='/default'>
-                    <button>Show All Movies</button>
-                </Link>
-            </div>
-            <p className='photoCredit'>Photo Credit: Karl Semilla</p>
+                <div className = 'searchBox'>
+                    <h1>Movie Browser</h1>
+                    <h3>Title: 
+                        <input type='text' name="titleIn"></input>
+                    </h3>
+                    <Link to='/default'>
+                        <button>Show Matching Movies</button>
+                    </Link>
+                    <Link to='/default'>
+                        <button>Show All Movies</button>
+                    </Link>
+                </div>
+                <p className='photoCredit'>Photo Credit: Karl Semilla</p>
             </div>
         )
 
